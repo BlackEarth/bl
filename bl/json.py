a4df7f86cfe996e2e728f648639b5d2d17818636
Sean Harrison <sah@blackearth.us>
2016-02-20 15:38:42 -0600

    debugged

--- bl/json.py
+++ bl/json.py
@@ -8,12 +8,18 @@ class JSON(File):
     def __init__(self, fn=None, data=None, **args):
         File.__init__(self, fn=fn, **args)
         if data is not None:
-            self.data = json.loads(self.read())
+            self.data = data
+        elif self.fn is not None and os.path.exists(self.fn):
+            self.data = self.read()
+        if type(self.data)==dict:
+            self.data = Dict(**self.data)
 
     def read(self):
-        return File.read(self, mode='r')
+        d = File.read(self, mode='r')
+        j = json.loads(d)
+        return j
 
-    def write(self, fn=None, data=None, indent=None, **args):
+    def write(self, fn=None, data=None, indent=2, **args):
         File.write(self, 
             fn=fn or self.fn, 
             data=json.dumps(data or self.data, indent=indent).encode('utf-8'), 
