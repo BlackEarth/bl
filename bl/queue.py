c20af86c758209b7eaa9ad8f15e89c2a70958132
Sean Harrison <sah@blackearth.us>
2016-02-20 14:42:17 -0600

    Queue is now script-language agnostic; it assumes '.json' in naming the queue entry file, but the new .process_entry() method doesn't actually do anything — it's intended to be overridden in a subclass.

--- bl/queue.py
+++ bl/queue.py
@@ -1,90 +1,96 @@
 
-import os, sys, subprocess, time, random, tempfile
+import os, sys, subprocess, time, traceback, random, tempfile
 from glob import glob
 from bl.dict import Dict
 from bl.text import Text
 
-"""There are other queue libraries; this one implements a simple file-based synchronous queue 
-that is processed at set intervals by the queue process.
+"""There are other queue libraries; this one implements a simple file-based queue 
+that is processed at set intervals by the queue process. 
+JSON is the default queue entry data extension, processed according to the specs of the subclass.
+Queue entry data is passed to the process_entry() method as bytes.
+If there is one queue process, it is a synchronous queue -- entries are processed FIFO.
+If there are multiple queue processes, it is asynchronous -- each process is synchronous, 
+but there is no synchronization between the processes; each process picks up the next entry.
 """
 
 class Queue(Dict):
 
-    def __init__(self, path, log=print, debug=False, **args):
-        if not os.path.exists(path):
-            os.makedirs(path)
-        Dict.__init__(self, path=path, log=log, debug=debug, **args)
+    def __init__(self, path, outpath=None, log=print, debug=False, **args):
+        if not os.path.exists(path): os.makedirs(path)
+        if outpath is None: outpath = path + '.OUT'
+        if not os.path.exists(outpath): os.makedirs(outpath)
+        Dict.__init__(self, path=path, outpath=outpath, log=log, debug=debug, **args)
+        self.log("[%s] init %r" % (self.timestamp(), self.__class__))
 
     def __repr__(self):
         return "%s(path='%s')" % (self.__class__.__name__, self.path)
 
+    def timestamp(self):
+        return time.strftime("%Y-%m-%d %H:%M:%S %Z")
+
     def listen(self, sleep=5):
         """listen to the queue directory, with a wait time in seconds between processing"""
         while True:
-            self.process()
+            self.process_queue()
             time.sleep(sleep)
 
-    def insert(self, script, prefix="", ext='.py'):
-        """insert the given script into the queue"""
-        qfn = os.path.join(self.path,
-                "%s%.5f-%.5f%s" % (prefix, time.time(), random.random(), ext))
-        qf = Text(fn=qfn, text=script)
+    def insert(self, text, prefix="", suffix="", ext='.json'):
+        """insert the given queue entry into the queue.
+        text    : the text of the queue entry (REQUIRED)
+        prefix  : a prefix for each queue entry filename (default '')
+        suffix  : a suffix for each queue entry filename (before extension, default '')
+        ext     : the extension of the queue entry filename (default '.json') 
+        """
+        qfn = os.path.join(self.path, "%s%s-%.5f%s%s" 
+            % (prefix, time.strftime("%Y%m%d.%H%M%S.%f"), random.random(), suffix, ext))
+        qf = Text(fn=qfn, text=text)
         qf.write()
         return qfn
 
     def list(self, pattern="*"):
-        """get a list of files currently in the queue."""
-        l = [fn for fn in 
-                glob(os.path.join(self.path, pattern))
-                if os.path.basename(fn)[0] != '!']
+        """get a list of files currently in the queue, sorted in order."""
+        l = [fn 
+            for fn in glob(os.path.join(self.path, pattern))
+            if not os.path.isdir(fn)]
         l.sort()
         return l
 
-    def process(self):
-        """process the items in the queue that are ready."""
+    def process_queue(self):
+        """Loop through all the currently-available queue entry files, in order; 
+        For each file, 
+            + read it, delete it (to prevent another process from reading), then process it
+            + if an exception happens processing an entry, log it and continue to the next item
+        """
         fns = self.list()
         for fn in fns:
-            stderr = tempfile.NamedTemporaryFile()
-            self.log("[%s] %s" % (log.timestamp(), fn))
+            self.log("[%s] %s" % (self.timestamp(), fn))
+
+            # ensure that another queue process has not and will not process this entry.
             try:
-                result = subprocess.check_call(
-                    [fn], universal_newlines=True, stderr=stderr)
-                if result != 0:
-                    newfn = self.rename(fn, prefix="!")
-                    self.log("result!=0:", newfn)
-                elif self.debug==True:
-                    self.rename(fn, prefix="!_")
-                else:
-                    os.remove(fn)
-            except subprocess.CalledProcessError as exception:
-                # rename the script file to suppress further execution attempts
-                newfn = self.rename(fn, prefix="!")
-                self.log("CalledProcessError:", newfn, sys.exc_info()[1])
-                self.handle_script_exception(scriptfn=newfn, errorfn=stderr.name)
+                newfn = os.path.join(outpath, os.path.basename(fn))
+                os.rename(fn, newfn)
+            except:
+                self.log("-- doesn't exist; already processed")
+                continue
 
-    def handle_script_exception(self, scriptfn=None, errorfn=None):
-        """handles exceptions that occur during queue script execution.
-        Default script exception handling:
-            * put the traceback into a file named scriptfn+".ERR" 
-                (scriptfn has already been renamed with !)
-            * also put the traceback into the queue log
-        Override this method for custom exception handling. Named parameters that are given:
-            scriptfn = the filesystem path to the queue script file
-            errorfn  = the filesystem path to the traceback file
+            # process this entry
+            try:
+                self.process_entry(newfn)
+            except exception:
+                self.handle_exception(fn=fn, exception=exception)
+
+    def process_entry(self, fn):
+        """override this method to define how your subclass queue processes entries.
+        fn      : the filename of the queue entry, which is probably in self.outpath
         """
-        if errorfn is not None and os.path.exists(errorfn):
-            errorf = open(errorfn, 'rb')
-            t = errorf.read(); errorf.close()
-            errorf = open(scriptfn+'.ERR', 'wb')
-            errorf.write(t); errorf.close()
-            if os.path.normpath(errorfn) != os.path.normpath(scriptfn+'.ERR') \
-            and os.path.exists(errorfn):
-                os.remove(errorfn)
-        self.log(t)
+        self.log("process_entry():", fn)
 
-    def rename(self, fn, prefix="!", suffix=''):
-        newfn = os.path.join(
-            self.path,
-            prefix + os.path.basename(fn) + suffix)
-        os.rename(fn, newfn)
-        return newfn
+    def handle_exception(self, fn=None, exception=exception):
+        """Handle exceptions that occur during queue script execution.
+        (Override this method to implement your own exception handling.)
+        fn          : the filename of the queue entry.
+        exception   : the exception object
+        """
+        self.log("== EXCEPTION:", fn)
+        self.log(traceback.format_exc())
+
