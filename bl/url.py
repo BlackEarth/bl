326cc529127b40550f682fc6a01d4a1b4965af61
Sean Harrison <sah@blackearth.us>
2016-04-06 22:03:46 -0500

    URL.finditer() uses re.finditer() to find urls in the given string and return them as match objects.

--- bl/url.py
+++ bl/url.py
@@ -1,6 +1,10 @@
 
-import urllib.parse
+import re, urllib.parse
 from bl.dict import Dict
+
+# pattern from https://mathiasbynens.be/demo/url-regex 
+URL_PATTERN = "(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?"
+URL_REGEXP = re.compile(pattern, re.I+re.U)
 
 class URL(Dict):
     """Sample usage:
@@ -100,6 +104,11 @@ class URL(Dict):
             return None
         else:
             return self.__class__(url=u)
+
+    @classmethod
+    def finditer(C, string):
+        """search the given string for URLs and return an iterator of matches."""
+        return URL_REGEXP.finditer(string)
 
     @classmethod
     def join(C, *args):
