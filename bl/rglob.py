2f15bc4a7c4a803c2a89d47612092afdbfd9c6cb
Sean Harrison <sah@blackearth.us>
2016-07-06 11:34:33 -0500

    rglob() now deals with directory non-existence without failing

--- bl/rglob.py
+++ bl/rglob.py
@@ -4,15 +4,18 @@ from glob import glob, escape
 
 def rglob(dirname, pattern, dirs=False, sort=True):
     """recursive glob, gets all files that match the pattern within the directory tree"""
-    fns = glob(os.path.join(dirname, pattern))
-    dns = [fn for fn 
-            in [escape(os.path.join(dirname, fn)) 
-                for fn in os.listdir(dirname)] 
-            if os.path.isdir(fn)]
-    if dirs==True:
-        fns += dns
-    for d in dns:
-        fns += rglob(d, pattern)
-    if sort==True:
-        fns.sort()
+    fns = []
+    if os.path.isdir(dirname):
+        fns = glob(os.path.join(dirname, pattern))
+        dns = [fn for fn 
+                in [escape(os.path.join(dirname, fn)) 
+                    for fn in os.listdir(dirname)] 
+                if os.path.isdir(fn)]
+        if dirs==True:
+            fns += dns
+        for d in dns:
+            if os.path.isdir(d):
+                fns += rglob(d, pattern)
+        if sort==True:
+            fns.sort()
     return fns
