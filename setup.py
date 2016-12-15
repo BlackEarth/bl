c999350f64e5a4cbd0694aa52e82862e8ff40c4a
Sean Harrison <sah@blackearth.us>
2016-12-15 09:56:00 -0600

    0.20.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 config = {
   "name": "bl",
-  "version": "0.19.0",
+  "version": "0.20.0",
   "description": "Black Earth core library",
   "url": "https://github.com/BlackEarth/bl",
   "author": "Sean Harrison",
