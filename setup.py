dcc7da5d7eb939b13c4f88698c685531d350325a
Sean Harrison <sah@blackearth.us>
2017-08-15 12:53:42 -0500

    1.7.0 – new functionality in the File class

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 config = {
   "name": "bl",
-  "version": "1.6.1",
+  "version": "1.7.0",
   "description": "Black Earth core library",
   "url": "https://github.com/BlackEarth/bl",
   "author": "Sean Harrison",
