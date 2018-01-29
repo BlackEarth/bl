0d5940c4815dbf8992d498c0e3a3d13398c0ab41
Sean Harrison <sah@bookgenesis.com>
2018-01-29 16:09:35 -0600

    Progress.report() returns float fractions

--- bl/progress.py
+++ bl/progress.py
@@ -11,40 +11,52 @@ class Progress(JSON):
 		# JSON.__init__() loads the stored progress data from the given .json file
 		super().__init__(fn=fn, data=data)
 		if self.data is None: self.data = Dict()
-		# self.current stores start times for current stack processes.
-		self.current = Dict()
+		# self.current_times stores start times for current stack processes.
+		self.current_times = Dict()
+		self.init_key = self.stack_key
 
 	def start(self, key=None):
 		"""initialize process timing for the current stack"""
 		key = key or self.stack_key
-		self.current[key] = time()
+		self.current_times[key] = time()
 
 	def runtime(self, key):
 		if self.data.get(key) is not None:
 			return sum(self.data[key]) / len(self.data[key])
 
-	def report(self, fraction=None):
-		"""report the total progress for the current stack, optionally given the local fraction completed"""
+	def runtimes(self):
 		r = Dict()
-		local_key = self.stack_key
-		for key in self.stack_keys:
-			if self.current.get(key) is None: 
-				self.start(key=key)
-			runtime = self.runtime(key)
-			if key == local_key and fraction is not None:
-				r[key] = "%.2f%%" % fraction
-			elif runtime is not None:
-				r[key] = "%.2f%%" % (100*(time() - self.current[key]) / runtime)
-			else:
-				r[key] = "%.2fs" % (time() - self.current[key])
+		for key in self.data.keys():
+			r[key] = self.runtime(key)
 		return r
 
-	def finish(self):
+	def report(self, fraction=None, runtimes=None):
+		"""report the total progress for the current stack, optionally given the local fraction completed.
+		fraction=None: if given, used as the fraction of the local method so far completed.
+		runtimes=None: if given, used as the expected runtimes for the current stack.
+		"""
+		r = Dict()
+		local_key = self.stack_key
+		runtimes = runtimes or self.runtimes()
+		for key in self.stack_keys:
+			if self.current_times.get(key) is None: 
+				self.start(key=key)
+			runtime = runtimes.get(key) or self.runtime(key)
+			if key == local_key and fraction is not None:
+				r[key] = fraction
+			elif runtime is not None:
+				r[key] = (time() - self.current_times[key]) / runtime
+			else:
+				r[key] = 0.00
+		return r
+
+	def finish(self, runtimes=None):
 		"""record the current stack process as finished"""
+		self.report(fraction=1.0, runtimes=runtimes)
 		key = self.stack_key
 		if self.data.get(key) is None:
 			self.data[key] = []
-		start_time = self.current.get(key) or time()
+		start_time = self.current_times.get(key) or time()
 		self.data[key].append(time() - start_time)
 
 	def __delete__(self):
@@ -56,13 +68,15 @@ class Progress(JSON):
 	@property
 	def stack_keys(self):
 		l = self.stack_key.split(',')
-		return list(reversed([','.join(l[:i]) for i in range(1, len(l)+1)]))
+		return [','.join(l[:i]) for i in range(1, len(l)+1)]
 
 	@property
 	def stack_key(self):
-		return ','.join([
+		return ','.join(list(reversed([
 			t.filename+':'+t.function for t in 
 			[inspect.getframeinfo(i.frame) for i in inspect.stack()]
 			if os.path.abspath(t.filename) != os.path.abspath(__file__)	# omit locations in this file
-		])
+			and t.function != '<module>'
+			and 'runpy.py' not in t.filename
+		])))
 
