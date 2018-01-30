9e97a8d7379a3e6acd814e1f86b835d7ec5d8574
Sean Harrison <sah@bookgenesis.com>
2018-01-29 23:15:59 -0600

    each runtime data object is now a dict with "runtime" and whatever params were included with __init__() or start(). (This allows us to easily override the runtime() method to use these parameters to calculate the estimated runtime. For example, if runtime is linear according to the size of the file being processed, we can include a filesize param at init or start, and then use the filesize of the current file, along with the filesizes and runtimes of past runs, to calculate the estimated runtime for this file.

--- bl/progress.py
+++ bl/progress.py
@@ -7,37 +7,34 @@ from bl.json import JSON
 class Progress(JSON):
 	"""tabulate progress statistics for given processes, and report the progress of those processes"""
 	
-	def __init__(self, fn=None, data=None):
+	def __init__(self, fn=None, data=None, **params):
 		# JSON.__init__() loads the stored progress data from the given .json file
-		super().__init__(fn=fn, data=data)
+		super().__init__(fn=fn, data=data, params=params)
 		if self.data is None: self.data = Dict()
-		# self.current_times stores start times for current stack processes.
-		self.current_times = Dict()
+		self.current_times = Dict()					# start times for current stack processes.
 		self.init_key = self.stack_key
 
-	def start(self, key=None):
+	def start(self, key=None, **params):
 		"""initialize process timing for the current stack"""
+		self.params.update(**params)
 		key = key or self.stack_key
 		self.current_times[key] = time()
 
 	def runtime(self, key):
 		if self.data.get(key) is not None:
-			return sum(self.data[key]) / len(self.data[key])
+			return sum([v['runtime'] for v in self.data[key]]) / len(self.data[key])
 
 	def runtimes(self):
-		r = Dict()
-		for key in self.data.keys():
-			r[key] = self.runtime(key)
-		return r
+		return Dict(**{key:self.runtime(key) for key in self.data.keys()})
 
-	def report(self, fraction=None, runtimes=None):
+	def report(self, fraction=None):
 		"""report the total progress for the current stack, optionally given the local fraction completed.
 		fraction=None: if given, used as the fraction of the local method so far completed.
 		runtimes=None: if given, used as the expected runtimes for the current stack.
 		"""
 		r = Dict()
 		local_key = self.stack_key
-		runtimes = runtimes or self.runtimes()
+		runtimes = self.runtimes()
 		for key in self.stack_keys:
 			if self.current_times.get(key) is None: 
 				self.start(key=key)
@@ -46,24 +43,16 @@ class Progress(JSON):
 				r[key] = fraction
 			elif runtime is not None:
 				r[key] = (time() - self.current_times[key]) / runtime
-			else:
-				r[key] = 0.00
 		return r
 
-	def finish(self, runtimes=None):
+	def finish(self):
 		"""record the current stack process as finished"""
-		self.report(fraction=1.0, runtimes=runtimes)
+		self.report(fraction=1.0)
 		key = self.stack_key
 		if self.data.get(key) is None:
 			self.data[key] = []
 		start_time = self.current_times.get(key) or time()
-		self.data[key].append(time() - start_time)
-
-	def __delete__(self):
-		"""save progress stats"""
-		if self.fn is not None:
-			self.write()
-		super().__delete__()
+		self.data[key].append(Dict(runtime=time()-start_time, **self.params))
 
 	@property
 	def stack_keys(self):
