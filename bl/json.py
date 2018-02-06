7887870c7a4b1836f1fcc00854ef60d6a4afe092
Sean Harrison <sah@bookgenesis.com>
2018-02-06 14:05:55 -0600

    JSON.__init__() and .write() both need to handle data polymorphically.

--- bl/json.py
+++ bl/json.py
@@ -9,9 +9,18 @@ class JSON(File):
 		if self.data is None and self.fn is not None and os.path.exists(self.fn):
 			self.data = self.read()
 		if self.data is not None:
-			self.data = json.loads(self.data)
+			if type(self.data)==bytes:
+				self.data = self.data.decode('utf-8')
+			if type(self.data)==str:
+				self.data = json.loads(self.data)
 
 	def write(self, fn=None, data=None, indent=2):
 		fn = fn or self.fn
-		data = data or json.dumps(self.data, indent=indent)
-		super().write(fn=fn, data=data.encode('utf-8'))
+		data = data or self.data
+		if type(data) == bytes:
+			d = data
+		elif type(data) == str:
+			d = data.encode('utf-8')
+		else:
+			d = json.dumps(data, indent=2).encode('utf-8')
+		super().write(fn=fn, data=d)
