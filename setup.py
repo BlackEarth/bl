40e61c905cdec2b3539cfd38ced8df76367ea2d6
Sean Harrison <sah@bookgenesis.com>
2018-04-18 16:34:52 -0500

    1.17.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 config = {
   "name": "bl",
-  "version": "1.16.2",
+  "version": "1.17.0",
   "description": "Black Earth core library",
   "url": "https://github.com/BlackEarth/bl",
   "author": "Sean Harrison",
