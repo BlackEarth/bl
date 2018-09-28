beba4f66273bb0c774711f9287e65d770866eba7
Sean Harrison <sah@bookgenesis.com>
2018-09-27 21:26:38 -0500

    revise bl.id a bit

--- bl/id.py
+++ bl/id.py
@@ -1,5 +1,5 @@
-
-"""Create a variety of different random ids.
+"""
+Create a variety of different random ids.
 
 id.random_id() returns a random_id with the given parameters.
 
@@ -13,8 +13,10 @@ non-discoverability for most circumstances. For example:
     + 62^16 = 4.77e28, which is huge 
     + 62^32 = 2.27e57, which is overkill by a lot -- we'll never get a repeat, and we can test for it.
 
-id.id_chars is id.alphanum_chars with confusing ones removed, so the set is a 
-respectable 54 characters long.
+id.b64_chars are the 64 characters allowed in url-safe base64 encoding
+    + 64^6 = 6.87e10
+    + 64^16 = 7.92e28
+    + 64^32 = 6.28e57
 
 id.punct_chars has most ascii punctuation.
 
@@ -31,58 +33,50 @@ in urls. 72 characters. This is useful for URL shorteners.
 
 import random
 
-lcase_chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
-                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
-                'w', 'x', 'y', 'z']
-
-hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-                'a', 'b', 'c', 'd', 'e', 'f',]
-
-alphanum_chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-                    'n','o','p','q','r','s','t','u','v','w','x','y','z', 
-                    'A','B','C','D','E','F','G','H','I','J','K','L','M',
-                    'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                    '0','1','2','3','4','5','6','7','8','9']
-
-id_chars = [i for i in alphanum_chars 
-            if i not in ['i','o','l','I','O','L','0','1']]
-    
-punct_chars = ['~','!','@','#','$','%','^','&','*','(',')','_','-','+',
-                '=','[','{',']','}','|',';',':',',','<','.','>','/','?']
-
+lcase_chars = (
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    + ['k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't']
+    + ['u', 'v', 'w', 'x', 'y', 'z']
+)
+ucase_chars = [c.upper() for c in lcase_chars]
+num_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+alpha_chars = lcase_chars + ucase_chars
+alphanum_chars = alpha_chars + num_chars
+b64_chars = alphanum_chars + ['-', '_']
+id_chars = b64_chars
+id_first_chars = alpha_chars + ['_']
+punct_chars = (
+    ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(']
+    + [')', '_', '-', '+', '=', '[', '{', ']', '}', '|']
+    + [';', ':', ',', '<', '.', '>', '/', '?']
+)
 ascii_chars = alphanum_chars + punct_chars
-
 urlslug_punct = ['-', '_', '.', '+', '!', '*', "'", '(', ')', ',']
 urlslug_chars = alphanum_chars + urlslug_punct
 slug_chars = urlslug_chars
 
-def random_id(length=8, charset=id_chars, first_charset=lcase_chars, group_char='', group_length=0, sep=None, group=None):
-    """Creates a random id with the given length and charset.
-        length=8                    the number of characters in the id
-        charset=id_chars            what character set to use (a list of characters)
-        first_charset=lcase_chars   what character set for the first character
-        group_char=''               what character to insert between groups
-        group_length=0              how long the groups are (default 0 means no groups)
-        sep                         alias for group_char
-        group                       alias for group_length
-    """
-    if sep is not None: 
-        group_char = sep
-    if group is not None:
-        group_length = group
 
+def random_id(length=16, charset=id_chars, first_charset=id_first_chars, sep='', group=0):
+    """Creates a random id with the given length and charset.
+    ## Parameters
+    * length          the number of characters in the id
+    * charset         what character set to use (a list of characters)
+    * first_charset   what character set for the first character
+    * sep=''          what character to insert between groups
+    * group=0         how long the groups are (default 0 means no groups)
+    """
     t = []
 
-    firstchars = list(set(charset).intersection(first_charset))
-    if len(firstchars)==0: 
-        firstchars = charset
+    first_chars = list(set(charset).intersection(first_charset))
+    if len(first_chars) == 0:
+        first_chars = charset
 
-    t.append(firstchars[random.randrange(len(firstchars))])
+    t.append(first_chars[random.randrange(len(first_chars))])
 
     for i in range(len(t), length):
-        if (group_length > 0) and (i % group_length == 0) and (i < length): 
-            t.append(group_char)
+        if (group > 0) and (i % group == 0) and (i < length):
+            t.append(sep)
         t.append(charset[random.randrange(len(charset))])
 
     return ''.join(t)
-
